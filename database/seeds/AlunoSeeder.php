52318ecaa5ecef227d9a95ce62fa49b192fb3976
AlvanirJr <alvanirjr95@gmail.com>
2018-07-29 23:16:24 -0300

    Cadastro de alunos

---
+++ database/seeds/AlunoSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AlunoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i = 1; $i < 4; $i++){
+    		DB::table('aluno')->insert(['nome' => str_random(10), 'cpf' => str_random(10), 'senha' => str_random(10), 'email' => , 'curso_id' => $i]);
+    	}
+    }
+}
