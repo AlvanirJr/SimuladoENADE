f4e73421ab4a6d259104bcdbb62a7ae37795edbe
AlvanirJr <alvanirjr95@gmail.com>
2018-08-09 13:55:07 -0300

    Logo inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,13 @@
 @section('content')
     
 
-<div class="col-sm-2 col-md-8 text-center">
-	<h1>Bem vindo</h1>
+<div class="row">
+	<div class="col-md-12 text-center">
+		<td align="left" valign="top">	<img src="1.png" width="250px"> </td>
+	</div>
+	
+
+	
 </div>
 
 
